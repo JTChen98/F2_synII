b12cbaad88812eae5830755f0269a1afbbfcd309
JTChen98 <chenjt38@mail2.sysu.edu.cn>
2024-07-23 23:42:44 +0800

    Update h2.estimate.R

--- h2.estimate.R
+++ h2.estimate.R
@@ -1,17 +1,22 @@
 require('hglm')
 
+# Loads the genotype of segregants and object 'cross' (contains average phenotype for each segregant for each trait).
 geno <- read.table('~/Desktop/BYxRM_GenoData.txt', header = TRUE)
 load('~/Desktop/cross_synII_res.Rdata')
 
 trait <- names(cross_synII_res$pheno)
 
+# The design matrix for the fixed effects.
 X <- t(as.matrix(geno[,-1] == 'R'))*1
 colnames(X) <- geno$marker
 X <- scale(X)
 A <- tcrossprod(X)/ncol(X)
 svdA <- svd(A)
+
+# The design matrix for the random effects.
 Z <- svdA$u %*% diag(sqrt(svdA$d))
 
+# Esitimate the narrow-sense heritability for each trait.
 pheno_synII_res <- cross_synII_res$pheno
 c <- target <- c()
 for (i in 1:length(trait)) {
