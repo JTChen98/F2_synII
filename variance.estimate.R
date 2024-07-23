c0a79a2ce9573e5d3d0c8ead3096b38d155297d0
JTChen98 <chenjt38@mail2.sysu.edu.cn>
2024-07-23 23:59:47 +0800

    Add files via upload

--- variance.estimate.R
+++ variance.estimate.R
@@ -1,13 +1,14 @@
+require(qtl)
+
+# Load the qtl list mapped by QTL.mapping.R
 qtl_list <- read.csv('~/Desktop/qtl.csv', header = T, stringsAsFactors = F)
-qtl_list <- qtl_list[,c(1:3)]
-colnames(qtl_list) <- c('trait', 'chr', 'pos')
 
-geno <- read.table('~/Desktop/BYxRM_GenoData.txt', header = T, stringsAsFactors = F)
+# Load the genotype of segregants and object 'cross' (contains average phenotype for each segregant for each trait).
+geno <- read.table('~/Desktop/Yeast/Round/1000seq/data/BYxRM_GenoData.txt', header = T, stringsAsFactors = F)
 load('~/Desktop/Yeast/Round/1000seq/data/cross_synII_res.Rdata')
-trait <- names(cross_synII_res$pheno)
 
-c <- target <- c()
-for(i in 1:length(trait){
+# Estimate the phenotypic variance explained by detected qtl for each trait
+for(i in 1:7){
   sug1 <- cross_synII_res
   sug1$pheno <- data.frame(cross_synII_res$pheno[[i]])
   out.em1 <- scanone(sug1, method = 'mr')
@@ -28,6 +29,7 @@ for(i in 1:length(trait){
   xx = xx =='R'
   xx = xx*1
   target <- data.frame(r2 = summary(lm(yy~xx))$r.squared, 
-                       se = sqrt(((summary(lm(yy~xx))$r.squared)^2)/qchisq(pf(summary(lm(yy~xx))$fstatistic[[1]],summary(lm(yy~xx))$fstatistic[[2]],summary(lm(yy~xx))$fstatistic[[3]],lower.tail=F),1,lower.tail=F)))
+                       se = sqrt(((summary(lm(yy~xx))$r.squared)^2)/qchisq(pf(summary(lm(yy~xx))$fstatistic[[1]],summary(lm(yy~xx))$fstatistic[[2]],summary(lm(yy~xx))$fstatistic[[3]],
+                       lower.tail=F), 1, lower.tail=F)))
   c <- rbind(c, target)
 }
