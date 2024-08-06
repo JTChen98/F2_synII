15901b9960c494275574d4e3e3244c7453e58b3b
JTChen98 <chenjt38@mail2.sysu.edu.cn>
2024-08-06 16:01:59 +0800

    Update GeneticEff.estimate.R

--- R/GeneticEff.estimate.R
+++ R/GeneticEff.estimate.R
@@ -1,3 +1,5 @@
+require(qtl)
+
 # location of gwasFx.R
 source('~/Desktop/Yeast/Round/BY4741_synII/gwasFx.R)
 
@@ -7,4 +9,19 @@ load('~/Desktop/Yeast/Round/BY4741_synII/synII/data/cross_synII_res.Rdata')
 load('~/Desktop/Yeast/Round/BY4741_synII/BY4741/data/cross_BY_res.Rdata')
 
 # Loads genotype data
-geno <- read.table('~/Desktop/Yeast/Round/1000seq/data/BYxRM_GenoData.txt', header = T)
+geno <- read.table('~/Desktop/Yeast/data/BYxRM_GenoData.txt', header = T)
+
+trait <- names(cross_synII_res$pheno)
+
+for(i in 1:7){
+  yy_synII <- scale(cross_synII_res$pheno[[i]])
+  naidx <- which(is.na(yy_synII))
+  if(length(naidx) > 0){
+    synII <- gwas(y = yy_synII[-naidx], X = xx[-naidx,])
+    }else{synII <- gwas(y = yy_synII, X = xx)}
+  yy_BY <- scale(cross_BY4741_res$pheno[[i]])
+  naidx <- which(is.na(yy_BY))
+  if(length(naidx) > 0){
+    BY4741 <- gwas(y = yy_BY[-naidx], X = xx[-naidx,])
+    }else{BY4741 <- gwas(y = yy_BY, X = xx)}
+}
